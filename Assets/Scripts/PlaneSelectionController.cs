1f18b37e6285767d2fefb95b221e1a0e2a541f81
Edward Ng <edwardlng@outlook.com>
2024-04-24 22:16:50 -0400

    Added Visual Feedback for Selections.

--- Assets/Scripts/PlaneSelectionController.cs
+++ Assets/Scripts/PlaneSelectionController.cs
@@ -16,7 +16,30 @@ using UnityEngine.XR.ARSubsystems;
 public class PlaneSelectionController : MonoBehaviour {
 
   /// <summary>
-  /// The ray cast manager attached to the AR session.
+  /// The default material for visualizing AR planes. Typically, it is the material attached to the
+  /// AR Default Plane prefab under the AR Plane Manager component.
+  /// </summary>
+  public Material defaultPlaneMaterial;
+
+  /// <summary>
+  /// The material to apply to a selected AR plane.
+  /// </summary>
+  public Material selectedPlaneMaterial;
+
+  /// <summary>
+  /// The currently selected plane.
+  /// </summary>
+  private ARPlane _plane;
+
+  /// <summary>
+  /// The plane manager attached to the AR session that detects and tracks flat surfaces in the
+  /// physical environment.
+  /// </summary>
+  private ARPlaneManager _planeManager;
+
+  /// <summary>
+  /// The raycast manager attached to the AR session that determines where a ray intersects with a
+  /// trackable object.
   /// </summary>
   private ARRaycastManager _raycastManager;
 
@@ -24,21 +47,44 @@ public class PlaneSelectionController : MonoBehaviour {
   /// Awake is called when a new instance of the behavior is created.
   /// </summary>
   private void Awake() {
+    _planeManager = GetComponent<ARPlaneManager>();
     _raycastManager = GetComponent<ARRaycastManager>();
   }
   
   /// <summary>
-  /// Selects an AR plane through AR ray casts based on a screen position.
+  /// Selects an AR plane through AR raycasts based on a screen position.
   /// </summary>
   /// <param name="screenPosition">
   /// The screen position to cast a ray and select a plane from.
   /// </param>
+  /// <remarks>
+  /// The method will always deselect the currently selected plane when called.
+  /// </remarks>
   public void HandleSelectPlane(Vector2 screenPosition) {
     List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+    
+    DeselectPlane();
+    
     if (_raycastManager.Raycast(screenPosition, hitResults, TrackableType.PlaneWithinPolygon)) {
-      Debug.Log("Touch at Plane.");
-    } else {
-      Debug.Log("Touch not at Plane.");
+      ARRaycastHit hit = hitResults[0];
+      TrackableId planeId = hit.trackableId;
+
+      ARPlane plane = _planeManager.GetPlane(planeId);
+      MeshRenderer planeMesh = plane.gameObject.GetComponent<MeshRenderer>();
+      planeMesh.material = selectedPlaneMaterial;
+
+      _plane = plane;
+    }
+  }
+
+  /// <summary>
+  /// Deselects the currently selected plane by returning the original material to the plane.
+  /// </summary>
+  private void DeselectPlane() {
+    if (_plane != null) {
+      MeshRenderer planeMesh = _plane.gameObject.GetComponent<MeshRenderer>();
+      planeMesh.material = defaultPlaneMaterial;
+      _plane = null;
     }
   }
 }
